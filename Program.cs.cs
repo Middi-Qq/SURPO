5e99ced241df21f9938c05a767582165a148dc7d
Middi-Qq <khabibullayev2007@mail.ru>
2025-12-12 14:47:58 +0300

    add gender

--- Program.cs.cs
+++ Program.cs.cs
@@ -12,15 +12,20 @@ public static class Program
     {
         public string Name { get; set; }
         public int Age { get; set; }
+        public bool Gender { get; set; } // 1 - Man, 0 - Woman
+
         public Person()
         {
             Name = "John";
             Age = 20;
+            Gender = true;
         }
-        public Person(string name, int age)
+
+        public Person(string name, int age, bool gender)
         {
             Name = name;
             Age = age;
+            Gender = gender;
         }
     }
 }
